769f88f7f0e20e8532b1077cef1a458fa081a110
Luke Thinnes <lukethinnes@yahoo.com>
2020-09-11 00:36:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,14 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+
+function sayHiToGrandma(string) {
+  switch sayHiToGrandma {
+    case (string)===lowercase:
+      return "I can't hear you!";
+    case (string)===uppercase:
+      return "YES INDEED!";
+    case (string)==="I love you, Grandma.":
+      return "I love you, too."
+  }
+}
